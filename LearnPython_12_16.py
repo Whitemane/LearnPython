6456603e6cd3283bb64e9138222f9170c386cccf
Whitemane <857117236@qq.com>
2021-12-17 00:02:52 +0800

    第一天代码

---
+++ LearnPython_12_16.py
@@ -0,0 +1,21 @@
+n1=30
+n2=-30
+n3=0
+print('默认是十进制',118)
+print('二进制',0b10101111)
+print('八进制',0o176)
+
+from decimal import Decimal
+print(Decimal('1.1')+Decimal('2.2'))
+
+
+x1=2.1
+x2=2.2
+print(x1+x2)
+
+name='撒'
+age=20
+print('工作有点不顺心'+name+str(age))
+
+N=input('今天想要什么礼物')
+print(N)
